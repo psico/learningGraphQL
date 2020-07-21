70a940ea57ee3e0a6310e6e7626dd3c540234ed3
psico <desenvolvedorjg@gmail.com>
2020-07-20 22:12:30 -0300

    [UPT] Importing the new schema.

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
-import { GraphQLServer } from "graphql-yoga";
+const { GraphQLServer } = require("graphql-yoga");
+const path = require("path");
 
 const server = new GraphQLServer({
-
+    typeDefs: path.resolve(__dirname, 'schema.graphql')
 });
 
 server.start();
