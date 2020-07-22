8512d40e8986886521cf0a797188d816e1315466
psico <desenvolvedorjg@gmail.com>
2020-07-22 19:23:20 -0300

    [ADD] Firts try to use MongoDB Memory Server.

--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
-const { GraphQLServer } = require("graphql-yoga");
+const {GraphQLServer} = require("graphql-yoga");
 const path = require("path");
 const resolvers = require("./resolvers");
 const mongoose = require("mongoose");
+import { MongoMemoryServer } from 'mongodb-memory-server';
 
-mongoose.connect("", {
-    useNewUrlParser: true
-});
+const mongod = new MongoMemoryServer();
+// const mongod = new MongoMemoryServer();
+mongoose.connect(mongod.getUri(), {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    connectTimeoutMS: 10000,
+})
+    .then(() => logger.info("Mongo DB Memory Server conectado com sucesso"))
+    .catch((err) => logger.error("Erro ao conectar ao Mongo DB Memory Server", err));
+;
 
 const server = new GraphQLServer({
     typeDefs: path.resolve(__dirname, 'schema.graphql'),
