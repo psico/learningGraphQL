eb69450f70e79a65349e688140f684687f6af30b
psico <desenvolvedorjg@gmail.com>
2020-07-24 19:46:28 -0300

    [UPT] Now is connecting on mongoDB memory server

--- src/server.js
+++ src/server.js
@@ -2,27 +2,22 @@ const {GraphQLServer} = require("graphql-yoga");
 const path = require("path");
 const resolvers = require("./resolvers");
 const mongoose = require("mongoose");
-const { MongoMemoryServer } = require('mongodb-memory-server');
+const {MongoMemoryServer} = require('mongodb-memory-server');
 
-const mongod = (async () => new MongoMemoryServer())().then( async (data) =>{
-console.log(await data.getUri());
-const uri = await data.getUri();
+(async () => new MongoMemoryServer())().then(async (data) => {
+    mongoose.connect(await data.getUri(), {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        connectTimeoutMS: 10000,
+    })
+        .then(() => console.log("Mongo DB Memory Server conectado com sucesso"))
+        .catch((err) => console.log("Erro ao conectar ao Mongo DB Memory Server", err));
 
-console.log(uri);
 
-mongoose.connect(uri, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    connectTimeoutMS: 10000,
-})
-    .then(() => logger.info("Mongo DB Memory Server conectado com sucesso"))
-    .catch((err) => logger.error("Erro ao conectar ao Mongo DB Memory Server", err));
-;
+    const server = new GraphQLServer({
+        typeDefs: path.resolve(__dirname, 'schema.graphql'),
+        resolvers
+    });
 
-const server = new GraphQLServer({
-    typeDefs: path.resolve(__dirname, 'schema.graphql'),
-    resolvers
-});
-
-server.start();
+    server.start();
 });
