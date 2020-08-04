3cb0b2719d1c190b659e429d0e47c274fcb0ffbc
psico <desenvolvedorjg@gmail.com>
2020-08-04 12:54:42 -0300

    [UPT] Resolver user mongoose now.

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,14 +1,15 @@
-const users = [
-    { id: 1, name: "João Gabriel", email: "joaogabriel@teste.com" },
-    { id: 2, name: "Virgília", email: "virgília@teste.com" }
-];
+// const users = [
+//     { id: 1, name: "João Gabriel", email: "joaogabriel@teste.com" },
+//     { id: 2, name: "Virgília", email: "virgília@teste.com" }
+// ];
+const User = require('./User')
 
 module.exports = {
     Query: {
-        users: () => users,
-        user: () => users[0]
+        users: () => User.find(),
+        user: (_, {id}) => User.findById(id)
     },
     Mutation: {
-        createUser: () => users[0]
+        createUser: (_, {name, email}) => User.create({name, email})
     }
 };
