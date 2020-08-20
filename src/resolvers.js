b98d178c15d4c7913ff0a776ad9704265cd98a11
psico <desenvolvedorjg@gmail.com>
2020-08-20 17:34:53 -0300

    [ADD] New resolvers for corrida

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,11 +1,14 @@
 const User = require('./User')
+const Corrida = require('./Corrida')
 
 module.exports = {
     Query: {
         users: () => User.find(),
-        user: (_, {id}) => User.findById(id)
+        user: (_, {id}) => User.findById(id),
+        corridas: () => Corrida.find()
     },
     Mutation: {
-        createUser: (_, {name, email}) => User.create({name, email})
+        createUser: (_, {name, email}) => User.create({name, email}),
+        criarCorrida: (_,{longitude, latitude}) => Corrida.create({longitude, latitude})
     }
 };
